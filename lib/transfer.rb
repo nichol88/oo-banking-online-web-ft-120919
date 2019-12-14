6b6772d6c20620588696ce7778edd73bb2b845d6
Brian Nicholls <nichol88@gmail.com>
2019-12-14 03:34:53 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,10 +1,13 @@
 class Transfer
   # your code here
+  attr attr_accessor :status
   attr_reader :sender, :receiver
   def initialize(from, to, amt)
     @sender = from
     @receiver = to
+    @status = "pending"
     from.balance -= amt
     to.balance += amt
+
   end
 end
